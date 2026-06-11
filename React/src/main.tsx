518ffff28472131286bc6d2261c5cdd15621c27a
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-12 00:57:47 +0300

    Update to 26.1 and apply devextreme-examples-template 26.1.3+ changes

--- React/src/main.tsx
+++ React/src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import config from 'devextreme/core/config';
 import './index.css';
 import App from './App.tsx';
+import { licenseKey } from './devextreme-license';
+
+config({ licenseKey });
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
--- Vue/src/main.ts
+++ Vue/src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue';
+import config from 'devextreme/core/config';
 import App from './App.vue';
 import router from './router';
-
 import './assets/main.css';
+import { licenseKey } from './devextreme-license';
+
+config({ licenseKey });
 
 const app = createApp(App);
 
